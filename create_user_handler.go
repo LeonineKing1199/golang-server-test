4176695444957ec5048420ec3b89ebcb7b57e15e
LeonineKing1199 <christian.mazakas@gmail.com>
2017-06-06 12:12:54 -0700

    Finally got JSON parsing to work

--- create_user_handler.go
+++ create_user_handler.go
@@ -10,14 +10,25 @@ import (
 // UserData is a simple datatype that we'll use
 // across the project
 type UserData struct {
-	firstName string
-	lastName  string
+	FirstName string
+	LastName  string
+}
+
+func send400(resWriter http.ResponseWriter, req *http.Request) {
+	resWriter.WriteHeader(http.StatusBadRequest)
+	resWriter.Write([]byte("Invalid data POST'd to server"))
+	return
 }
 
 // CreateUser adds a user to our database, using the global
 // DBHandler as specified in db_connect.go
 func CreateUser(addUser *sql.Stmt) func(http.ResponseWriter, *http.Request) {
 	return func(resWriter http.ResponseWriter, req *http.Request) {
+		if req.Method != "POST" {
+			http.NotFound(resWriter, req)
+			return
+		}
+
 		decoder := json.NewDecoder(req.Body)
 
 		var userData UserData
@@ -25,12 +36,19 @@ func CreateUser(addUser *sql.Stmt) func(http.ResponseWriter, *http.Request) {
 		err := decoder.Decode(&userData)
 		if err != nil {
 			fmt.Println(err.Error())
-			resWriter.WriteHeader(http.StatusBadRequest)
-			resWriter.Write([]byte("Invalid data POST'd to server"))
+			send400(resWriter, req)
 			return
 		}
 
 		defer req.Body.Close()
+
+		firstName := userData.FirstName
+		lastName := userData.LastName
+
+		if len(firstName) == 0 || len(lastName) == 0 {
+			send400(resWriter, req)
+			return
+		}
 
 		resWriter.WriteHeader(http.StatusOK)
 	}
--- static_content/scripts/test.js
+++ static_content/scripts/test.js
@@ -5,30 +5,39 @@
     const url      = 'http://localhost:8080/users';
     const isAsync  = true;
 
-    const sendRequest = (xhr, firstName, lastName, cb) => {
+    const sendRequest = (xhr, FirstName, LastName, cb) => {
         xhr.onreadystatechange = cb;
         xhr.open(method, url, isAsync);
-        xhr.send({ firstName, lastName });
+        xhr.setRequestHeader('Content-type','application/json');
+        xhr.send(JSON.stringify({ FirstName, LastName }));
     };
 
-    const handleBtnSubmit = () => {
+    const handleBtnSubmit = (btn) => () => {
         const firstNameDomNode = document.getElementById('first-name-input');
         const lastNameDomNode  = document.getElementById('last-name-input');     
 
-        const firstName = firstNameDomNode.value;
-        const lastName  = lastNameDomNode.value;
+        const firstName = firstNameDomNode.value || '';
+        const lastName  = lastNameDomNode.value  || '';
+
+        if (!firstName || !lastName) {
+            return;
+        }
 
         const xhr = new XMLHttpRequest();
         sendRequest(xhr, firstName, lastName, () => {
+            btn.removeAttribute('disabled');
+
             if (xhr.readyState == XMLHttpRequest.DONE) {
                 console.log(`request complated with: ${xhr.status}`);
             }
         });
+
+        btn.setAttribute('disabled', '');
     };
 
     document.addEventListener('DOMContentLoaded', () => {
         const addUserBtn = document.getElementById('add-user-btn');
 
-        addUserBtn.addEventListener('mouseup', handleBtnSubmit);
+        addUserBtn.addEventListener('mouseup', handleBtnSubmit(addUserBtn));
     });
 })();
